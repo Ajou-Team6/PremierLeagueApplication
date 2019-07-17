1dff5393d221dcdc6155830f87ad3bd5fe033745
GoonerL <daddy2@ajou.ac.kr>
2019-07-18 03:14:48 +0900

    PersonalDetailsSort Modified - Ascend or Descend Sort

--- src/main/java/com/github/ajouteam6/premierleague/sort/PersonalDetailsSort.java
+++ src/main/java/com/github/ajouteam6/premierleague/sort/PersonalDetailsSort.java
@@ -1,6 +1,122 @@
 package com.github.ajouteam6.premierleague.sort;
 
 
+import com.github.ajouteam6.premierleague.domain.player.Player;
+
+import java.util.Arrays;
+
 //명수
 public class PersonalDetailsSort {
+    // 명수 - 이름 기준 정렬
+    public void sortByPlayerName(Player[] playerList, int sortSelect){
+        if(sortSelect==0)  Arrays.sort(playerList,this::compareByNameDescend);
+        else Arrays.sort(playerList,this::compareByNameAscend);
+        for(Player players : playerList){
+            System.out.println(players.getPersonalDetails().getName());
+        }
+    }
+    // 명수 - 선수 소속팀 기준 정렬
+    public void sortByPlayerTeam(Player[] playerList,int sortSelect){
+        if(sortSelect==0)  Arrays.sort(playerList,this::compareByTeamDescend);
+        else Arrays.sort(playerList,this::compareByTeamAscend);
+        for(Player players:playerList){
+            System.out.println(players.getPersonalDetails().getName()+ " : " +
+                    players.getPersonalDetails().getTeam());
+        }
+    }
+    // 명수 - 선수 등번호 기준 정렬
+    public void sortByPlayerBackNumber(Player[] playerList, int sortSelect){
+        if(sortSelect==0)  Arrays.sort(playerList,this::compareByBackNumberDescend);
+        else Arrays.sort(playerList,this::compareByBackNumberAscend);
+        for(Player players:playerList){
+            System.out.println(players.getPersonalDetails().getName()+ " : " +
+                    players.getPersonalDetails().getBackNumber());
+        }
+    }
+    // 명수 - 선수 포지션 기준 정렬
+    public void sortByPlayerPosition(Player[] playerList,int sortSelect){
+        if(sortSelect==0)  Arrays.sort(playerList,this::compareByPositionDescend);
+        else Arrays.sort(playerList,this::compareByPositionAscend);
+        for(Player players:playerList){
+            System.out.println(players.getPersonalDetails().getName()+ " : " +
+                    players.getPersonalDetails().getPosition());
+        }
+    }
+    // 명수 - 선수 나이 기준 정렬
+    public void sortByPlayerAge(Player[] playerList,int sortSelect){
+        if(sortSelect==0)  Arrays.sort(playerList,this::compareByAgeDescend);
+        else Arrays.sort(playerList,this::compareByAgeAscend);
+        for(Player players:playerList){
+            System.out.println(players.getPersonalDetails().getName()+ " : " +
+                    players.getPersonalDetails().getAge());
+        }
+    }
+
+    // 명수 - 선수 키 기준 정렬
+    public void sortByPlayerHeight(Player[] playerList,int sortSelect){
+        if(sortSelect==0)  Arrays.sort(playerList,this::compareByHeightDescend);
+        else Arrays.sort(playerList,this::compareByHeightAscend);
+        for(Player players:playerList){
+            System.out.println(players.getPersonalDetails().getName()+ " : " +
+                    players.getPersonalDetails().getHeight()+"cm");
+        }
+    }
+
+
+
+    public int compareByNameDescend(Player p1, Player p2){
+        if(p2.getPersonalDetails().getName().compareTo(p1.getPersonalDetails().getName())<0){
+            return -1;
+        }
+        return 1;
+    }
+    public int compareByNameAscend(Player p1, Player p2){
+        if(p2.getPersonalDetails().getName().compareTo(p1.getPersonalDetails().getName())<0){
+            return 1;
+        }
+        return -1;
+    }
+    public int compareByTeamAscend(Player p1, Player p2){
+        if(p2.getPersonalDetails().getTeam().compareTo(p1.getPersonalDetails().getTeam())<0){
+            return 1;
+        }
+        return -1;
+    }
+    public int compareByTeamDescend(Player p1, Player p2){
+        if(p2.getPersonalDetails().getTeam().compareTo(p1.getPersonalDetails().getTeam())<0){
+            return -1;
+        }
+        return 1;
+    }
+    public int compareByBackNumberAscend(Player p1, Player p2){
+        return p1.getPersonalDetails().getBackNumber()-p2.getPersonalDetails().getBackNumber();
+    }
+    public int compareByBackNumberDescend(Player p1, Player p2){
+        return p2.getPersonalDetails().getBackNumber()-p1.getPersonalDetails().getBackNumber();
+    }
+    public int compareByPositionDescend(Player p1, Player p2){
+        if(p2.getPersonalDetails().getPosition().compareTo(p1.getPersonalDetails().getPosition())<=0){
+            return -1;
+        }
+        return 1;
+    }
+    public int compareByPositionAscend(Player p1, Player p2){
+        if(p2.getPersonalDetails().getPosition().compareTo(p1.getPersonalDetails().getPosition())<=0){
+            return 1;
+        }
+        return -1;
+    }
+
+    public int compareByAgeAscend(Player p1, Player p2){
+        return p1.getPersonalDetails().getAge()-p2.getPersonalDetails().getAge();
+    }
+    public int compareByAgeDescend(Player p1, Player p2){
+        return p2.getPersonalDetails().getAge()-p1.getPersonalDetails().getAge();
+    }
+    public int compareByHeightAscend(Player p1, Player p2){
+        return p1.getPersonalDetails().getHeight()-p2.getPersonalDetails().getHeight();
+    }
+    public int compareByHeightDescend(Player p1, Player p2){
+        return p2.getPersonalDetails().getHeight()-p1.getPersonalDetails().getHeight();
+    }
 }
--- src/test/java/com/github/ajouteam6/premierleague/sort/PersonalDetailsSortTest.java
+++ src/test/java/com/github/ajouteam6/premierleague/sort/PersonalDetailsSortTest.java
@@ -1,6 +1,166 @@
 package com.github.ajouteam6.premierleague.sort;
 
+import com.github.ajouteam6.premierleague.domain.player.AttackStats;
+import com.github.ajouteam6.premierleague.domain.player.DefenceStats;
+import com.github.ajouteam6.premierleague.domain.player.PersonalDetails;
+import com.github.ajouteam6.premierleague.domain.player.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 //명수
 public class PersonalDetailsSortTest {
+    Player Harry_Kane;
+    Player Eden_Hazard;
+    Player Sadio_Mane;
+    Player Alexis_Sanchez;
+    Player Alexander_Lacazette;
+    Player[] playerList = new Player[5];
+    @Before
+    public void 선수_정보_설정(){
+        Harry_Kane = Player.builder()
+                .attackStats(new AttackStats(22,10,100,68,1203))
+                .defenceStats(new DefenceStats(11,18,7,6,10,2))
+                .personalDetails(new PersonalDetails("Harry Kane","Tottenham Hotspurs",9,"Striker","England",25,185))
+                .build();
+        Eden_Hazard = Player.builder()
+                .attackStats(new AttackStats(20,17,90,61,1501))
+                .defenceStats(new DefenceStats(8,10,4,5,1,2))
+                .personalDetails(new PersonalDetails("Eden Hazard","Chelsea FC",10,"Forward","Belguim",27,176))
+                .build();
+        Sadio_Mane = Player.builder()
+                .attackStats(new AttackStats(22,4,94,58,1556))
+                .defenceStats(new DefenceStats(15,12,5,10,5,7))
+                .personalDetails(new PersonalDetails("Sadio Mane","Liverpool FC", 10, "Left Forward", "Senegal",26,177))
+                .build();
+        Alexander_Lacazette = Player.builder()
+                .attackStats(new AttackStats(13,10,78,50,1120))
+                .defenceStats(new DefenceStats(5,12,8,4,6,3))
+                .personalDetails(new PersonalDetails("Alexander Lacazette","Arsenal FC",9,"Center Forward","France",25,181))
+                .build();
+        Alexis_Sanchez = Player.builder()
+                .attackStats(new AttackStats(2,5,52,28,506))
+                .defenceStats(new DefenceStats(2,5,3,1,8,0))
+                .personalDetails(new PersonalDetails("Alexis Sanchez","Manchester United",7,"Left Winger","Chile",30,180))
+                .build();
+        playerList[0]=Harry_Kane;
+        playerList[1]=Eden_Hazard;
+        playerList[2]=Sadio_Mane;
+        playerList[3]=Alexander_Lacazette;
+        playerList[4]=Alexis_Sanchez;
+    }
+    //명수 - 이름 기준 정렬 테스트
+    @Test
+    public void 이름기준_정렬이_내림차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerName(playerList,0);
+        for(int i=0;i<playerList.length-1;i++){
+            assertEquals(-1, (personalDetailssort.compareByNameDescend(playerList[i], playerList[i + 1])));
+        }
+    }
+    @Test
+    public void 이름기준_정렬이_오름차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerName(playerList,1);
+        for(int i=0;i<playerList.length-1;i++){
+            assertEquals(-1, (personalDetailssort.compareByNameAscend(playerList[i], playerList[i + 1])));
+        }
+    }
+
+    // 명수 - 선수 소속 팀 기준 정렬 테스트
+    @Test
+    public void 선수소속팀기준_정렬이_내림차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerTeam(playerList,0);
+        for(int i=0;i<playerList.length-1;i++){
+            assertEquals(-1, (personalDetailssort.compareByTeamDescend(playerList[i], playerList[i + 1])));
+        }
+    }
+    @Test
+    public void 선수소속팀기준_정렬이_오름차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerTeam(playerList,1);
+        for(int i=0;i<playerList.length-1;i++){
+            assertEquals(-1, (personalDetailssort.compareByTeamAscend(playerList[i], playerList[i + 1])));
+        }
+    }
+
+    // 명수 - 선수 등 번호 기준 정렬 테스트
+    @Test
+    public void 선수등번호기준정렬이_내림차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerBackNumber(playerList,0);
+        for(int i=0;i<playerList.length-1;i++){
+            assertTrue(playerList[i].getPersonalDetails().getBackNumber()>=playerList[i+1].getPersonalDetails().getBackNumber());
+        }
+    }
+    @Test
+    public void 선수등번호기준정렬이_오름차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerBackNumber(playerList,1);
+        for(int i=0;i<playerList.length-1;i++){
+            assertTrue(playerList[i].getPersonalDetails().getBackNumber()<=playerList[i+1].getPersonalDetails().getBackNumber());
+        }
+    }
+
+    // 명수 - 선수 포지션 기준 정렬 테스트
+    @Test
+    public void 선수포지션기준_정렬이_내림차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerPosition(playerList,0);
+        for(int i=0;i<playerList.length-1;i++){
+            assertEquals(-1, (personalDetailssort.compareByPositionDescend(playerList[i], playerList[i + 1])));
+        }
+    }
+    @Test
+    public void 선수포지션기준_정렬이_오름차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerPosition(playerList,1);
+        for(int i=0;i<playerList.length-1;i++){
+            assertEquals(-1, (personalDetailssort.compareByPositionAscend(playerList[i], playerList[i + 1])));
+        }
+    }
+
+    // 명수 - 선수 나이 기준 정렬 테스트
+    @Test
+    public void 선수나이기준정렬이_내림차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerAge(playerList,0);
+        for(int i=0;i<playerList.length-1;i++){
+            assertTrue(playerList[i].getPersonalDetails().getAge()>=playerList[i+1].getPersonalDetails().getAge());
+        }
+    }
+    @Test
+    public void 선수나이기준정렬이_오름차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerAge(playerList,1);
+        for(int i=0;i<playerList.length-1;i++){
+            assertTrue(playerList[i].getPersonalDetails().getAge()<=playerList[i+1].getPersonalDetails().getAge());
+        }
+    }
+
+    // 명수 - 선수 키 기준 정렬 테스트
+    @Test
+    public void 선수키기준정렬이_내림차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerHeight(playerList,0);
+        for(int i=0;i<playerList.length-1;i++){
+            assertTrue(playerList[i].getPersonalDetails().getHeight()>=playerList[i+1].getPersonalDetails().getHeight());
+        }
+    }
+    @Test
+    public void 선수키기준정렬이_오름차순이_아니면_에러() {
+        PersonalDetailsSort personalDetailssort = new PersonalDetailsSort();
+        personalDetailssort.sortByPlayerHeight(playerList,1);
+        for(int i=0;i<playerList.length-1;i++){
+            assertTrue(playerList[i].getPersonalDetails().getHeight()<=playerList[i+1].getPersonalDetails().getHeight());
+        }
+    }
+
+
+
+
 
 }
