71f1c766aead8a07f2a82779facce8152ea42555
Jong in Park <whddls9632@naver.com>
2019-07-17 15:36:44 +0900

    Add two player comparing methods with attack point.

--- src/main/java/com/github/ajouteam6/premierleague/service/PlayerCompare.java
+++ src/main/java/com/github/ajouteam6/premierleague/service/PlayerCompare.java
@@ -1,5 +1,28 @@
 package com.github.ajouteam6.premierleague.service;
 
+import com.github.ajouteam6.premierleague.domain.Player;
+import lombok.Builder;
+import lombok.Data;
+
 // 종인
+@Builder
+@Data
 public class PlayerCompare {
+
+    public Player twoPlayerCompareByGoal(Player firstPlayer, Player secondPlayer) {
+        if (firstPlayer.getAttackStats().getGoals() > secondPlayer.getAttackStats().getGoals())
+            return firstPlayer;
+        return secondPlayer;
+    }
+
+    public Player twoPlayerCompareByAttackPoint(Player firstPlayer, Player secondPlayer) {
+        int firstPlayerAttackPoint = getPlayerAttackPoint(firstPlayer);
+        int secondPlayerAttackPoint = getPlayerAttackPoint(secondPlayer);
+        if (firstPlayerAttackPoint > secondPlayerAttackPoint) return firstPlayer;
+        return secondPlayer;
+    }
+
+    private int getPlayerAttackPoint(Player firstPlayer) {
+        return firstPlayer.getAttackStats().getGoals() + firstPlayer.getAttackStats().getAssists();
+    }
 }
--- src/test/java/com/github/ajouteam6/premierleague/service/PlayerCompareTest.java
+++ src/test/java/com/github/ajouteam6/premierleague/service/PlayerCompareTest.java
@@ -1,10 +1,19 @@
 package com.github.ajouteam6.premierleague.service;
 
 import com.github.ajouteam6.premierleague.domain.Player;
+import lombok.extern.log4j.Log4j;
 import org.junit.Test;
+import sun.rmi.runtime.Log;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
 
 // 종인
+@Log4j
 public class PlayerCompareTest {
+    PlayerCompare playerCompare = PlayerCompare.builder().build();
+
     Player Heungmin_Son = Player.builder()
             .personalDetails(new Player.PersonalDetails("Son Heung-Min", "Tottenham Hotspur", 7, "Forward", "Republic of Korea", 23))
             .attackStats(new Player.AttackStats(12, 6,74, 29, 25))
@@ -18,8 +27,13 @@ public class PlayerCompareTest {
             .build();
 
     @Test
-    public void twoPlayerCompareTest() {
+    public void twoPlayerCompareByGoalTest() {
+        assertThat(playerCompare.twoPlayerCompareByGoal(Heungmin_Son, Harry_kane), sameInstance(Harry_kane));
 
     }
 
+    @Test
+    public void twoPlayerCompareByAttackPoint() {
+        assertThat(playerCompare.twoPlayerCompareByAttackPoint(Heungmin_Son, Harry_kane), sameInstance(Harry_kane));
+    }
 }
