588f47773e0c5db154a4846fd8ac2747decb68dd
younho9 <dusgh121@gmail.com>
2019-07-17 14:18:54 +0900

    Add Builder annotation into Player class

--- src/main/java/com/github/ajouteam6/premierleague/domain/Player.java
+++ src/main/java/com/github/ajouteam6/premierleague/domain/Player.java
@@ -1,12 +1,19 @@
 package com.github.ajouteam6.premierleague.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 //연호
 @Data
+@Builder
 public class Player {
 
+    PersonalDetails personalDetails;
+    AttackStats attackStats;
+    DefenceStats defenceStats;
+
     @Data
+    @Builder
     public static class PersonalDetails {
         private String name;
         private String team;
@@ -17,6 +24,7 @@ public class Player {
     }
 
     @Data
+    @Builder
     public static class AttackStats {
         private int goals;
         private int assists;
@@ -26,6 +34,7 @@ public class Player {
     }
 
     @Data
+    @Builder
     public static class DefenceStats {
         private int cleanSheets;
         private int tackles;
