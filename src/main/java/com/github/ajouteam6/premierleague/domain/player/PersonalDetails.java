25461cf5072203192065357ba13b480bc827d2b3
GoonerL <daddy2@ajou.ac.kr>
2019-07-17 20:11:51 +0900

    Player Domain Modified - AllArgsConstructor

--- src/main/java/com/github/ajouteam6/premierleague/domain/player/PersonalDetails.java
+++ src/main/java/com/github/ajouteam6/premierleague/domain/player/PersonalDetails.java
@@ -1,11 +1,13 @@
 package com.github.ajouteam6.premierleague.domain.player;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 //명수
 @Data
 @Builder
+@AllArgsConstructor
 public class PersonalDetails {
     private String name;
     private String team;
