89db1cd1fe5e5d53059424aa60ab3808c12d8d1d
younho9 <dusgh121@gmail.com>
2019-07-17 18:37:23 +0900

    create setup player data

--- src/test/java/com/github/ajouteam6/premierleague/domain/PlayerTest.java
+++ src/test/java/com/github/ajouteam6/premierleague/domain/PlayerTest.java
@@ -1,7 +1,43 @@
 package com.github.ajouteam6.premierleague.domain;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayerTest {
 
+    //연호
+    private List<Player> PlayerList = new ArrayList<Player>();
+
+    @Before
+    public void setUp() {
+        Player salah = new Player.PlayerBuilder()
+                .personalDetails(new Player.PersonalDetails.PersonalDetailsBuilder()
+                        .name("Mohamed Salah")
+                        .team("Liverpool")
+                        .backNumber(11)
+                        .position("Forward")
+                        .nationality("Egypt")
+                        .age(27)
+                        .build()
+                )
+                .attackStats(new Player.AttackStats.AttackStatsBuilder()
+                        .goals(56)
+                        .assists(19)
+                        .shots(304)
+                        .shotsOnTarget(138)
+                        .passes(2268)
+                        .build()
+                )
+                .defenceStats(new Player.DefenceStats.DefenceStatsBuilder()
+                        .cleanSheets(15)
+                        .tackles(21)
+                        .interception(5)
+                        .dualsWon(143)
+                        .dualLost(283)
+                        .clearances(4)
+                        .build()
+                ).build();
+    }
 }
